7ec8d5bc47966e7124f2b2fd9d89a676322b1ae5
prasa-dd-vp <prasad.venkatesh90@gmail.com>
2021-09-24 21:13:24 -0400

    Updated standalone program

--- bitcoin_miner_standalone.fsx
+++ bitcoin_miner_standalone.fsx
@@ -17,15 +17,17 @@ let mutable count = 0
 //creating an actor system
 let system = ActorSystem.Create("FSharp")
 
+let gatorId = "venkateshpramani"
+
 //union of messages to an actor
-type ActorMsg =
+type Command =
     | StartMining
     | MineCoins of int*int
     | PrintCoins of list<string>
     | StopMining
 
 //Print actor to print the input string and hash value
-let PrintingActor (mailbox:Actor<_>)=
+let Printer (mailbox:Actor<_>)=
     let rec loop()=actor{
         let! msg = mailbox.Receive()
         match msg with
@@ -36,24 +38,16 @@ let PrintingActor (mailbox:Actor<_>)=
         return! loop()
     }
     loop()
-let printRef = spawn system "PrintingActor" PrintingActor 
-
-// let getValidHash input = generateRandomString >>
-
-// let generateRandomString input = fun 
-
-// let genereateHash = fun
-
-// let isValidHash = fun (x:string,y:int) -> int64 ("0x" + x.[0..y-1]) = 0L
+let printRef = spawn system "Printer" Printer 
 
 //Miner actor
-let MinerActor (mailbox:Actor<_>)=
+let Miner (mailbox:Actor<_>)=
     let rec loop()=actor{
         let! msg = mailbox.Receive()
         match msg with 
         | MineCoins(zeroCount, actorId) ->  let hashObj = SHA256Managed.Create()
-                                            for i in 0 .. actorCount .. 1000000 do
-                                                let  input = "venkateshpramani"+ string (i+actorId)
+                                            for i in 0 .. actorCount .. 100000000 do
+                                                let  input = gatorId + string (i+actorId)
                                                 let hashString = input 
                                                                 |> System.Text.Encoding.ASCII.GetBytes 
                                                                 |> hashObj.ComputeHash
@@ -61,7 +55,7 @@ let MinerActor (mailbox:Actor<_>)=
                                                                 |> Seq.reduce(+)
                                                 let isValidHash = int64 ("0x" + hashString.[0..zeroCount-1]) = 0L
                                                 
-                                                if isValidHash then printRef <! PrintCoins([hashString; input])
+                                                if isValidHash then printRef <! PrintCoins([input; hashString])
                                             
                                             mailbox.Sender() <! StopMining
 
@@ -70,17 +64,17 @@ let MinerActor (mailbox:Actor<_>)=
     loop()
 
 //Master Actor
-let MasterActor (mailbox:Actor<_>) =
+let Master (mailbox:Actor<_>) =
     let rec loop()=actor{
         let! msg = mailbox.Receive()
         match msg with 
-        | StartMining -> let actorRefList = [for i in 1 .. actorCount do yield(spawn system ("Actor" + (string i)) MinerActor)] //creating workers
-                         for i in 0 .. actorCount-1 do //distributing work to the workers
-                            actorRefList.Item(i) <! MineCoins(zeroCount, i) //sending message to worker
+        | StartMining ->    let actorRefList = [for i in 1 .. actorCount do yield(spawn system ("Actor" + (string i)) Miner)] //creating workers
+                            for i in 0 .. actorCount-1 do //distributing work to the workers
+                                actorRefList.Item(i) <! MineCoins(zeroCount, i) //sending message to worker
 
-        | StopMining -> count <- count + 1
-                        if count = actorCount then 
-                            mailbox.Context.System.Terminate() |> ignore
+        | StopMining ->     count <- count + 1
+                            if count = actorCount then 
+                                mailbox.Context.System.Terminate() |> ignore
                                             
         | _ -> printfn "Master actor received a wrong message"
         return! loop()
@@ -89,7 +83,7 @@ let MasterActor (mailbox:Actor<_>) =
 
 
 //creating master actor
-let actorRef = spawn system "MasterActor" MasterActor
+let actorRef = spawn system "Master" Master
 actorRef <! StartMining
 
 //waiting for boss actor to terminate the actor system
